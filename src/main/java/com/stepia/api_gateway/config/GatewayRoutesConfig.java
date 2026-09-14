3bc3ca8541bd25808c25ff5209942de616ce61de
Ludwig-Ivan <ivansierras11@gmail.com>
2026-09-14 07:56:48 -0600

    Reconfiguracion del enrutamiento de peticiones

--- src/main/java/com/stepia/api_gateway/config/GatewayRoutesConfig.java
+++ src/main/java/com/stepia/api_gateway/config/GatewayRoutesConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
 import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.rewritePath;
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.setPath;
 import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
@@ -21,7 +22,7 @@ public class GatewayRoutesConfig {
                                 .route(
                                                 path("/api/core-service/**"),
                                                 http())
-                                .before(uri("http://localhost:8081"))
+                                .before(uri("http://stepia-core-service:8081"))
                                 .before(rewritePath(
                                                 "/api/core-service/(?<segment>.*)",
                                                 "/${segment}"))
@@ -35,10 +36,33 @@ public class GatewayRoutesConfig {
                                 .route(
                                                 path("/api/document-service/**"),
                                                 http())
-                                .before(uri("http://localhost:8082"))
+                                .before(uri("http://stepia-document-service:8082"))
                                 .before(rewritePath(
                                                 "/api/document-service/(?<segment>.*)",
                                                 "/${segment}"))
                                 .build();
         }
+
+        @Bean
+        public RouterFunction<ServerResponse> iaServiceRoute() {
+
+                return route("ia-service")
+                                .route(
+                                                path("/api/ia-service/**"),
+                                                http())
+                                .before(uri("http://stepia-ia-service:8083"))
+                                .before(rewritePath(
+                                                "/api/ia-service/(?<segment>.*)",
+                                                "/${segment}"))
+                                .build();
+        }
+
+        // @Bean
+        // public RouterFunction<ServerResponse> iaPredictRoute() {
+        // return route("ia-predict")
+        // .POST("/api/ia-service/predict", http())
+        // .before(uri("http://stepia-ia-service:8083"))
+        // .before(setPath("/predict"))
+        // .build();
+        // }
 }
