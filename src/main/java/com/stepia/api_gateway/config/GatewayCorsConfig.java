2cd4e1ccb99a39310ce2ec8f916766321b46d295
Ludwig-Ivan <ivansierras11@gmail.com>
2026-09-15 20:39:02 -0600

    Reacomodo de rutas en ENV para produccion y develop

--- src/main/java/com/stepia/api_gateway/config/GatewayCorsConfig.java
+++ src/main/java/com/stepia/api_gateway/config/GatewayCorsConfig.java
@@ -1,5 +1,6 @@
 package com.stepia.api_gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,10 +12,13 @@ import java.util.List;
 @Configuration
 public class GatewayCorsConfig {
 
+    @Value("gateway.env.allowed-origins")
+    private String allowedOrigins;
+
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(List.of("*"));
+        corsConfig.setAllowedOrigins(List.of(allowedOrigins.split(",")));
         corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE",
                 "OPTIONS", "PATCH"));
         corsConfig.setAllowedHeaders(List.of("*"));
